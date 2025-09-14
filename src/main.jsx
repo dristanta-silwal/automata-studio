c11b6218cbec370f58c553903b139feb061516d4
Dristanta Silwal <dristantasilwal003@gmail.com>
2025-09-14 14:21:17 -0700

    main changed

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AutomatonProvider } from './store/AutomatonContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AutomatonProvider>
         <App />
       </AutomatonProvider>
